72f613e3be620e84ff4927d975ce6052ba5ad456
zhaozp24 <zhaozp24@midea.com>
2022-09-19 17:50:23 +0800

    可处理行索引为字母的数据

--- 缺失值.py
+++ 缺失值.py
@@ -3,10 +3,10 @@ import pandas as pd
 #循环所有需要处理的列
 def data_prepare(df):
    data = df
-   list1 = ['power_fans','power_frrr']
-   for c in list1:
+   n_col = data.shape[1]
+   for c in range(n_col):
       data_processing = pd.DataFrame()
-      data_processing['data'] = data.loc[:, c].copy()
+      data_processing['data'] = data.iloc[:, c].copy()
 
       # 对于异常值在一定条件下进行填充
       nan_r, _ = np.where(data_processing == 0)
@@ -16,15 +16,16 @@ def data_prepare(df):
 
       nan_list = []
       for k, g in df2:
-         find_list = list(g.loc[:, 1])
+         find_list = list(g.iloc[:, 1])
          if len(find_list) < 4:
             nan_list = nan_list + find_list
+
       for i in nan_list:
-         data.loc[i, c] = data.loc[i - 1, c]
+         data.iloc[i,c] = data.iloc[i-1,c]
    return (data)
 power_merge=pd.DataFrame()
 power_merge['power_fans']=[1,2,3,5,0,6,0,0,4,5,0,0,0,7,5]
 power_merge['power_frrr']=[1,0,3,5,0,0,0,0,4,5,0,1,8,7,5]
-print(power_merge)
+power_merge.index = ['a','s', 'd','f', 'g', 'h','j','k','l','q','w','e','r','t','y']
 power_merge_new = data_prepare(power_merge)
 print(power_merge_new)
